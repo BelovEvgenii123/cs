19b4b36ace2eb7d489e2823f155c92c764c9d9ac
evgenijbelov <beles123@bk.ru>
2022-11-22 00:58:03 +0300

    z4

---
+++ z4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите третье число");
+int c = int.Parse(Console.ReadLine());
+int m ;
+m=a;
+if (b>a)
+m= b;
+if (c>m)
+m= c ;
+Console.Write(m);
