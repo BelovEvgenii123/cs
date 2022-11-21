8384323e5f3bad084625b692f45c1c5d771aa9d8
evgenijbelov <beles123@bk.ru>
2022-11-21 21:59:32 +0300

    added z2

---
+++ z2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите первое число");
+Int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+Int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+Console.Write(a=="max");
+else 
+Console.Write(b=="min");
+
