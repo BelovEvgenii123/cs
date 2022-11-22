08631526ee9aca998e62c497991a768190afc4a5
evgenijbelov <beles123@bk.ru>
2022-11-22 15:41:33 +0300

    z6

---
+++ z6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите первое число");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+    Console.Write ("Четное");
+else
+    Console.Write ("Не четное");
+
