319f72baa7e1ac56db91deb0985ed7e6fdd8e43c
evgenijbelov <beles123@bk.ru>
2022-11-22 15:29:53 +0300

    z2

--- z2/Program.cs
+++ z2/Program.cs
@@ -3,6 +3,14 @@ Console.WriteLine("Напишите первое число");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Напишите второе число");
 int b = int.Parse(Console.ReadLine());
+while (a==b)
+{
+   Console.WriteLine("Числа равны, Введите заново") ;
+Console.WriteLine("Напишите первое число");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Напишите второе число");
+b = int.Parse(Console.ReadLine());
+}
 if (a>=b)
 {
 Console.Write("max="+ a);
