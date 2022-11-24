89ee5c2f5ae2474be4252a04e4393b5b0ca1ec43
evgenijbelov <beles123@bk.ru>
2022-11-24 18:34:36 +0300

    все кроме 13

---
+++ z13/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая выводит третью цифру(c начала) заданного числа или сообщает, что третьей цифры нет.
+//НЕ ИСПОЛЬЗОВАТЬ НУМЕРАЦИЮ СТРОК
+
+Console.Clear();
+Console.WriteLine("Напишите число");
+int a = int.Parse(Console.ReadLine());
+
+int [] array ={a}
+Console.WriteLine(array[2]);
---
+++ z15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Напишите число");
+int a = int.Parse(Console.ReadLine());
+if ((a <6) && (a!=0))
+Console.WriteLine("нет");
+if ((a>5) && (a<8))
+Console.WriteLine("да");
+if ((a>7)||(a==0))
+Console.WriteLine("нет такого дня недели");
